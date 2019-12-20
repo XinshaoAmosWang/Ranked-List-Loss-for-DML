8ff539a7c39c2c94c9e5c33c37a8a55e17aab1b8
XinshaAmosWang <xinshaowang@gmail.com>
2019-12-20 11:34:11 +0000

    updates

---
+++ download.sh
@@ -0,0 +1,3 @@
+git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-DML.git
+
+git pull origin master
---
+++ update.sh
@@ -0,0 +1,5 @@
+git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-Deep-Metric-Learning.git
+
+git add .
+git commit -m "updates"
+git push origin master
