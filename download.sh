1aa9775e643e1b5a7ba299f06c067553db465d93
XinshaAmosWang <xinshaowang@gmail.com>
2019-12-20 13:57:30 +0000

    How to train on CUB dataset as inquired

--- download.sh
+++
@@ -1,3 +0,0 @@
-git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-DML.git
-
-git pull origin master
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-Deep-Metric-Learning.git
 
 git add .
-git commit -m "How to train on CUB dataset as requested"
+git commit -m "How to train on CUB dataset as inquired"
 git push origin master
