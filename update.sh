4c40bab5ef328afaf1cfbfc1970d3b90883734c7
XinshaoAmosWang <xinshaowang@gmail.com>
2020-03-05 23:35:55 +0000

    The previous links of slide and poster are not valid now!

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-Deep-Metric-Learning.git
 
 git add .
-git commit -m "How to train on CUB dataset as inquired"
+git commit -m "The previous links of slide and poster are not valid now!"
 git push origin master
