3253939a96cc7a53c1bdb4e19bd31bb6c36dbbb5
XinshaoAmosWang <xinshaowang@gmail.com>
2020-05-08 08:42:13 +0100

    TPAMI extension with the main target to further improve this work

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git remote set-url origin git@github.com:XinshaoAmosWang/Ranked-List-Loss-for-Deep-Metric-Learning.git
 
 git add .
-git commit -m "The previous links of slide and poster are not valid now!"
+git commit -m "TPAMI extension with the main target to further improve this work"
 git push origin master
